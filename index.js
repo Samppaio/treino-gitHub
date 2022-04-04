4b29e8dd48b951b5073ee0c7d84ffccc42e7001e
Rafael Sampaio <rafael.sampa1989@gmail.com>
2022-04-04 08:16:51 -0300

    aplicacao de soma

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-let num1 = 2
-let num2 = 5
+let num1 = 10
+let num2 = 23
 
-function sum(num1, num2){
-	return a + b
+function sum() {
+  let total = num1 + num2;
+  return total;
 }
 
-console.log(sum())
+console.log(sum());
